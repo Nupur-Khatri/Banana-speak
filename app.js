aec0bae58a8b0877be819c85a83d53c9660046ff
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-06-09 16:51:58 +0530

    Add - final edits and styling

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?"+ "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wronh in server! try again after sometime")
+}
+
 function clickHandler() {
-    outputDiv.innerText = "ajajajajajajaja " + txtinput.value; 
+    var inputText = txtinput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        })
+    .catch(errorHandler)
 };
 
 btntranslate.addEventListener("click", clickHandler)
+
+
+
